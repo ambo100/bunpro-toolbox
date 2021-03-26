cb33e6a5464e9f1760a806fda5032bb7f9096bb1
ambo100 <ambo102@yahoo.co.uk>
2021-03-26 10:17:42 +0000

    Add files via upload

---
+++ js/lunr-data.js
@@ -0,0 +1,401 @@
+window.lunrData = {
+  "index": {
+    "version": "1.0.0",
+    "fields": [
+      {
+        "name": "longname",
+        "boost": 1000
+      },
+      {
+        "name": "name",
+        "boost": 500
+      },
+      {
+        "name": "tags",
+        "boost": 300
+      },
+      {
+        "name": "kind",
+        "boost": 110
+      },
+      {
+        "name": "title",
+        "boost": 100
+      },
+      {
+        "name": "summary",
+        "boost": 70
+      },
+      {
+        "name": "description",
+        "boost": 50
+      },
+      {
+        "name": "body",
+        "boost": 1
+      }
+    ],
+    "ref": "id",
+    "tokenizer": "default",
+    "documentStore": {
+      "store": {
+        "index.html": [
+          "bunpro",
+          "document",
+          "index",
+          "readm",
+          "toolbox",
+          "userscript"
+        ],
+        "global.html": [
+          "customis",
+          "document",
+          "global",
+          "grammar",
+          "link",
+          "page",
+          "search"
+        ]
+      },
+      "length": 2
+    },
+    "tokenStore": {
+      "root": {
+        "docs": {},
+        "b": {
+          "docs": {},
+          "u": {
+            "docs": {},
+            "n": {
+              "docs": {},
+              "p": {
+                "docs": {},
+                "r": {
+                  "docs": {},
+                  "o": {
+                    "docs": {
+                      "index.html": {
+                        "ref": "index.html",
+                        "tf": 300
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "d": {
+          "docs": {},
+          "o": {
+            "docs": {},
+            "c": {
+              "docs": {},
+              "u": {
+                "docs": {},
+                "m": {
+                  "docs": {},
+                  "e": {
+                    "docs": {},
+                    "n": {
+                      "docs": {},
+                      "t": {
+                        "docs": {
+                          "index.html": {
+                            "ref": "index.html",
+                            "tf": 35
+                          },
+                          "global.html": {
+                            "ref": "global.html",
+                            "tf": 600
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "i": {
+          "docs": {},
+          "n": {
+            "docs": {},
+            "d": {
+              "docs": {},
+              "e": {
+                "docs": {},
+                "x": {
+                  "docs": {
+                    "index.html": {
+                      "ref": "index.html",
+                      "tf": 1300
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "r": {
+          "docs": {},
+          "e": {
+            "docs": {},
+            "a": {
+              "docs": {},
+              "d": {
+                "docs": {},
+                "m": {
+                  "docs": {
+                    "index.html": {
+                      "ref": "index.html",
+                      "tf": 110
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "t": {
+          "docs": {},
+          "o": {
+            "docs": {},
+            "o": {
+              "docs": {},
+              "l": {
+                "docs": {},
+                "b": {
+                  "docs": {},
+                  "o": {
+                    "docs": {},
+                    "x": {
+                      "docs": {
+                        "index.html": {
+                          "ref": "index.html",
+                          "tf": 300
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "u": {
+          "docs": {},
+          "s": {
+            "docs": {},
+            "e": {
+              "docs": {},
+              "r": {
+                "docs": {},
+                "s": {
+                  "docs": {},
+                  "c": {
+                    "docs": {},
+                    "r": {
+                      "docs": {},
+                      "i": {
+                        "docs": {},
+                        "p": {
+                          "docs": {},
+                          "t": {
+                            "docs": {
+                              "index.html": {
+                                "ref": "index.html",
+                                "tf": 35
+                              }
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "c": {
+          "docs": {},
+          "u": {
+            "docs": {},
+            "s": {
+              "docs": {},
+              "t": {
+                "docs": {},
+                "o": {
+                  "docs": {},
+                  "m": {
+                    "docs": {},
+                    "i": {
+                      "docs": {},
+                      "s": {
+                        "docs": {
+                          "global.html": {
+                            "ref": "global.html",
+                            "tf": 14
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "g": {
+          "docs": {},
+          "l": {
+            "docs": {},
+            "o": {
+              "docs": {},
+              "b": {
+                "docs": {},
+                "a": {
+                  "docs": {},
+                  "l": {
+                    "docs": {
+                      "global.html": {
+                        "ref": "global.html",
+                        "tf": 1410
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          },
+          "r": {
+            "docs": {},
+            "a": {
+              "docs": {},
+              "m": {
+                "docs": {},
+                "m": {
+                  "docs": {},
+                  "a": {
+                    "docs": {},
+                    "r": {
+                      "docs": {
+                        "global.html": {
+                          "ref": "global.html",
+                          "tf": 14
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        "l": {
+          "docs": {},
+          "i": {
+            "docs": {},
+            "n": {
+              "docs": {},
+              "k": {
+                "docs": {
+                  "global.html": {
+                    "ref": "global.html",
+                    "tf": 14
+                  }
+                }
+              }
+            }
+          }
+        },
+        "p": {
+          "docs": {},
+          "a": {
+            "docs": {},
+            "g": {
+              "docs": {},
+              "e": {
+                "docs": {
+                  "global.html": {
+                    "ref": "global.html",
+                    "tf": 14
+                  }
+                }
+              }
+            }
+          }
+        },
+        "s": {
+          "docs": {},
+          "e": {
+            "docs": {},
+            "a": {
+              "docs": {},
+              "r": {
+                "docs": {},
+                "c": {
+                  "docs": {},
+                  "h": {
+                    "docs": {
+                      "global.html": {
+                        "ref": "global.html",
+                        "tf": 14
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      },
+      "length": 13
+    },
+    "corpusTokens": [
+      "bunpro",
+      "customis",
+      "document",
+      "global",
+      "grammar",
+      "index",
+      "link",
+      "page",
+      "readm",
+      "search",
+      "toolbox",
+      "userscript"
+    ],
+    "pipeline": [
+      "trimmer",
+      "stopWordFilter",
+      "stemmer"
+    ]
+  },
+  "store": {
+    "index.html": {
+      "id": "index.html",
+      "kind": "readme",
+      "title": "Bunpro Toolbox",
+      "longname": "index",
+      "name": "Bunpro Toolbox",
+      "tags": "index",
+      "summary": "Userscript Documentation",
+      "description": "",
+      "body": ""
+    },
+    "global.html": {
+      "id": "global.html",
+      "kind": "global",
+      "title": "Documentation",
+      "longname": "global",
+      "name": "Documentation",
+      "tags": "global",
+      "summary": "Customise the search links for Grammar pages",
+      "description": "",
+      "body": ""
+    }
+  }
+};
